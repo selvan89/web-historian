b7e2b32c3b4a1f8c7f0ac1b5d519401c16f8f020
selvan89 <selvanprograms@gmail.com>
2015-01-07 15:15:11 -0800

    setting up tests

--- web/request-handler.js
+++ web/request-handler.js
@@ -3,5 +3,8 @@ var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
+  if(req.url === '/' && req.method === 'GET'){
+    console.log('hi');
+  }
   res.end(archive.paths.list);
 };
